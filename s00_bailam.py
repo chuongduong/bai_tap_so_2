27e3db566890832d525b87e53159342a2d363068
Chuong Duong Nguyen <37611992-chuongduong0601@users.noreply.replit.com>
2024-11-21 09:43:45 +0000

    bai 2

--- s00_bailam.py
+++ s00_bailam.py
@@ -30,6 +30,17 @@ hi('1', '22', '333', '4444') | Hi 1, 22, 333, and 4444!
 #endregion debai
 
 #region bailam
-def hi(*args):
-   return 'TODO'
+def hi(*args, name=None):
+   if not args and not name:  # Neu khong có gia tri thi se Hi 
+      return "Hi!"
+   if name:
+      return f"Hi {name}!"   # neus có gia tri thi hi  + name 
+   args = [str(arg) for arg in args if arg]   # for gia tri theo vong lap
+   if not args:
+      return "Hi!"           # neu khong co gia tri thi hi 
+   if len(args) == 1:
+      return f"Hi {args[0]}!"    # tim duoc 1 gia trị thi Hi + name 
+   if len(args) == 2:
+      return f"Hi {args[0]}, and {args[1]}!"   
+   return f"Hi {', '.join(args[:-1])}, and {args[-1]}!"
 #endregion bailam
